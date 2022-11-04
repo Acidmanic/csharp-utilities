b6134ac6a73cd0525233f133ebeda7602eecaafd
Mani Moayedi <acidmanic.moayedi.com>
2022-11-04 22:46:09 +0330

    Add test project

---
+++ Acidmanic.Utilities.Tdd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acidmanic.Utilities.Tdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
