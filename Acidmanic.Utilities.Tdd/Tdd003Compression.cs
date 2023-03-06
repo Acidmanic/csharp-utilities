a53894c62292d3e85dc125efd506aff71d64eea9
Mani Moayedi <acidmanic.moayedi.com>
2023-03-07 03:10:50 +0330

    Clean up compression code and example tdd code

--- Acidmanic.Utilities.Tdd/Tdd003Compression.cs
+++ Acidmanic.Utilities.Tdd/Tdd003Compression.cs
@@ -13,51 +13,14 @@ namespace Acidmanic.Utilities.Tdd
         public override void Main()
         {
             
-            var original = "This is a very not compressed value";
+            var original = "This is a very not compressed value\n";
 
-            var data = Encoding.Unicode.GetBytes(original);
-            
-            var sourceStream = new MemoryStream(data);
+            original += original;
+            original += original;
+            original += original;
 
-            var outStream = new MemoryStream();
-            
-            var compressionStream = new GZipStream(outStream,CompressionLevel.Fastest);
-
-            sourceStream.CopyTo(compressionStream);
-            
-            compressionStream.Flush();
-
-            data = outStream.ToArray();
-
-            compressionStream.Dispose();
-            
-            outStream.Dispose();
-            
-            sourceStream.Dispose();
-
-            var converted = Convert.ToBase64String(data);
-
-
-            var reBytes = Convert.FromBase64String(converted);
-            
-            var reSourceStream = new MemoryStream(reBytes);
-
-            var reOutStream = new MemoryStream();
-
-            var decompressStream = new GZipStream(reSourceStream, CompressionMode.Decompress);
-            
-            decompressStream.CopyTo(reOutStream);
-            reOutStream.Flush();
-
-            reBytes = reOutStream.GetBuffer();
-
-            var reConstructed = Encoding.Unicode.GetString(reBytes);
-            
-            
             var method = Compressions.Brotli;
             
-            
-
             var start = DateTime.Now;
             
             var compressed = original.CompressAsync(method).Result;
@@ -72,12 +35,12 @@ namespace Acidmanic.Utilities.Tdd
             
             Console.WriteLine($"Compression and Decompression using {method}");
             
-            Console.WriteLine($"Original: {original}");
+            Console.WriteLine($"Original: {original.Length}\n {original}");
             
-            Console.WriteLine($"Compressed: {compressed}, took {compressionTime.TotalMilliseconds} ms");
+            Console.WriteLine($"Compressed: {compressed.Length}\n {compressed}, took {compressionTime.TotalMilliseconds} ms");
             
-            Console.WriteLine($"Decompressed: {decompressed}, took {deCompressionTime.TotalMilliseconds} ms");
-            
+            Console.WriteLine($"Decompressed: {decompressed.Length}\n {decompressed}, took {deCompressionTime.TotalMilliseconds} ms");
+                        
         }
     }
 }
